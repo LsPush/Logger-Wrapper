d2691f95826ab7afee90f98108920b1fee9c46ae
tomeokin <tome.okin@gmail.com>
2017-01-26 09:33:21 +0800

    1.update build tools; 2. update License 3. clean project

--- logger-wrapper/src/main/java/com/decay/logger/AndroidLog.java
+++ logger-wrapper/src/main/java/com/decay/logger/AndroidLog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 TomeOkin
+ * Copyright 2017 LsPush
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
